bc4aab309a75d1cd53c7c0a302d0463443e22a93
carwynteifion <carwynteifion@yahoo.co.uk>
2022-02-10 01:45:06 +0000

    Add register, login and logout templates and routing

--- app.py
+++ app.py
@@ -26,32 +26,99 @@ coll_recipes = mongo.db.recipes
 @app.route("/")
 @app.route("/get_recipes")
 def get_recipes():
+    """
+    Pulls recipes list from the DB and displays on rendered
+    home page.
+    """
     recipes = list(coll_recipes.find())
     return render_template("recipes.html", recipes=recipes)
 
 
-@app.route("/register.html", methods=["GET", "POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    """
+    Pulls up register page for users to create an account.
+    Validates inputs for username and password and posts
+    to database before redirecting to user recipes page.
+    """
     if request.method == "POST":
         existing_user = coll_users.find_one(
-            {"username": request.form.get("username".lower()})
-        
+            {"username": request.form.get("username".lower())})
+
+        # check if user exists already
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
-        register = {
+
+        # check username length
+        if len(request.form.get("username")) < 5 or len(request.form.get("username")) > 20:
+            flash("Usernames must be between 5 and 20 characters long")
+            return redirect(url_for("register"))
+        
+        # check password length
+        if len(request.form.get("password")) < 8 or len(request.form.get("password")) > 20:
+            flash("Passwords must be between 8 and 20 characters long")
+            return redirect(url_for("register"))
+
+        # populates and posts JSON object to DB
+        user = {
             "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
+            "password": generate_password_hash(request.form.get("password")),
+            "recipes": [],
+            "favourites": []
         }
-        coll_users.insert_one(register)
+        coll_users.insert_one(user)
 
         # put the new user into session cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful")
-        return redirect(url_for("my_recipes", username=session["user"]))
+        return redirect(url_for("get_recipes", username=session["user"]))
 
     return render_template("register.html")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """
+    Checks inputted username and password against DB list.
+    If username and password both match, logs the user
+    into a session and redirects to their My Recipes page.
+    """
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "get_recipes", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """
+    Logs user out of session
+    """
+    flash("Successfully logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 
