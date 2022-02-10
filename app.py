845b4bf15f0af0af5d0ec53de671f102e297e662
carwynteifion <carwynteifion@yahoo.co.uk>
2022-02-10 03:00:13 +0000

    Add routing and functionality for adding recipes

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ mongo = PyMongo(app)
 # Collections
 coll_users = mongo.db.users
 coll_recipes = mongo.db.recipes
+coll_cuisines = mongo.db.cuisines
 
 
 @app.route("/")
@@ -86,7 +87,7 @@ def login():
     """
     if request.method == "POST":
         # check if username exists in db
-        existing_user = mongo.db.users.find_one(
+        existing_user = coll_users.find_one(
             {"username": request.form.get("username").lower()})
         
         if existing_user:
@@ -119,6 +120,57 @@ def logout():
     flash("Successfully logged out")
     session.pop("user")
     return redirect(url_for("login"))
+
+
+@app.route("/add_recipe", methods=["GET", "POST"])
+def add_recipe():
+    """
+    Adds recipe to database
+    """
+    if "user" in session:
+        if request.method == "POST":
+            # Ingredients and Prep converted into lists
+            ingredients = request.form.get("ingredients").splitlines()
+            method = request.form.get("method").splitlines()
+
+            # Creates JSON entry for recipe
+            recipe = {
+                "recipeName": request.form.get("recipe_name"),
+                "cuisine": request.form.get("cuisine"),
+                "method": method,
+                "ingredients": ingredients,
+                "createdBy": session["user"]
+            }
+            coll_recipes.insert_one(recipe)
+            flash("Recipe Successfully Added")
+            return redirect(url_for("get_recipes"))
+
+        cuisines = coll_cuisines.find().sort("cuisine_name", 1)
+        return render_template("addrecipe.html", cuisines=cuisines)
+
+    else:
+        flash("You must be logged in to do this!")
+        return redirect(url_for("login"))
+
+@app.route("/view_recipe")
+def view_recipe():
+    """
+    View recipe
+    """
+
+
+@app.route("/update_recipe")
+def update_recipe():
+    """
+    Updates recipe details in database
+    """
+
+
+@app.route("/delete_recipe")
+def delete_recipe():
+    """
+    Deletes recipe from database
+    """
 
 
 
